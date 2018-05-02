cda53725a6eb484b2e252a8acd5caad39d30f11a
mvgiambrone <mgiambro@caltech.edu>
2018-05-02 14:55:07 -0700

    . to .. in app.test.js

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import rawConfig from './config.json';
+import rawConfig from '../config.json';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
