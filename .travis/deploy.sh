e974c9b912882120c6131333e9e061ee97d647dc
Aw Young Qingzhuo <hello@veniversum.me>
2018-05-02 18:22:53 -0700

    Update travis deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "Deploying app to server."
+pwd 
 eval "$(ssh-agent -s)"
 chmod 600 .travis/id_rsa
 ssh-add .travis/id_rsa
 
-ssh ${SSH_USER}@${SSH_HOST} <<EOF
-	  cd ${DEPLOY_DIR}
-	  git pull origin master && npm ci && npm run build
-	EOF
+ssh -tt ${SSH_USER}@${SSH_HOST} <<-EOF
+	cd ${DEPLOY_DIR}
+	git pull origin master && npm ci && npm run build
+EOF
