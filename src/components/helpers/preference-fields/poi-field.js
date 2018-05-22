9b759c9307c165147fd1447b713c815f3c004afd
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2018-05-22 12:26:59 -0700

    Make tags in preference editor more user-friendly

--- src/components/helpers/preference-fields/poi-field.js
+++ src/components/helpers/preference-fields/poi-field.js
@@ -5,8 +5,10 @@
  */
 
 import ReactTags from 'react-tag-autocomplete';
+import 'react-select/dist/react-select.css';
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
 import _ from 'lodash';
 
 import {PreferenceSchema, PoiTypes} from '../../../util/preferences-state';
@@ -25,49 +27,41 @@ export default class PoiField extends Component {
         this.fieldName = this.fieldData.name;
 
         this.state = {
-            tags: [],
-            suggestions: [],
+            poiOptions: [],
+            poiSelectValue: [],
         };
 
         const initialPois = this.props.currentPrefState.get(this.fieldName);
         _.forEach(PoiTypes, (poiType) => {
-            const tagObject = {id: poiType.name, name: poiType.displayName};
+            const optionObject = {value: poiType.name, label: poiType.displayName};
+            this.state.poiOptions.push(optionObject);
             if (initialPois[poiType.name]) {
-                this.state.tags.push(tagObject);
-            } else {
-                this.state.suggestions.push(tagObject);
+                this.state.poiSelectValue.push(optionObject);
             }
         });
 
         this.toggle = this.toggle.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     toggle(enabled) {
-        if (enabled) this.props.updatePreference(this.fieldName, PoiField.getPoisObject(this.state.tags));
+        if (enabled) this.props.updatePreference(this.fieldName, PoiField.getPoisObject(this.state.poiSelectValue));
         else this.props.updatePreference(this.fieldName, null);
     }
 
-    handleDelete(i) {
-        const tags = this.state.tags.slice(0);
-        tags.splice(i, 1);
-        this.setState({tags});
-    }
-
-    handleAddition(tag) {
-        const tags = [].concat(this.state.tags, tag);
-        this.setState({tags});
-    }
+	onChange(value) {
+		this.setState({poiSelectValue: value});
+		console.log('Numeric Select value changed to', value);
+    } 
 
     componentWillUpdate(nextProps, nextState) {
-        if (this.state.tags !== nextState.tags) {
-            this.props.updatePreference(this.fieldName, PoiField.getPoisObject(nextState.tags));
-        }
+        this.props.updatePreference(this.fieldName, PoiField.getPoisObject(nextState.poiSelectValue));
     }
 
-    static getPoisObject(tags) {
+    static getPoisObject(selectValue) {
         const poiObject = {};
-        for (const tag of tags) {
-            poiObject[tag.id] = 1;
+        for (const option of selectValue) {
+            poiObject[option.value] = 1;
         }
         return poiObject;
     }
@@ -76,11 +70,12 @@ export default class PoiField extends Component {
         return (
             <PreferenceField fieldData={this.fieldData} toggle={this.toggle}>
                 <div className="has-text-left is-size-7">
-                    <ReactTags
-                        tags={this.state.tags}
-                        suggestions={this.state.suggestions}
-                        handleDelete={this.handleDelete.bind(this)}
-                        handleAddition={this.handleAddition.bind(this)}/>
+                    <Select
+                        options={this.state.poiOptions}
+                        value={this.state.poiSelectValue}
+                        onChange={this.onChange}
+                        matchPos="any"
+                        multi={true}/>
                 </div>
             </PreferenceField>
         );
